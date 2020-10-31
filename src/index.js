6ea75e709e0f1e6b20bde741c33a097a4ab138fe
Steven <steven@outfit.io>
2020-10-31 18:55:36 +1000

    my second attempt to create a multipage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'typeface-ibm-plex-sans';
 
 ReactDOM.render(
   <React.StrictMode>
+  <Fragment>
     <App {...window.templateProps} />
+    <App {...window.templateProps} />
+    <App {...window.templateProps} />
+  </Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
