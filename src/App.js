8a396d6cd46012fa43f2acaef86b639569b83dab
Steven <steven@outfit.io>
2020-11-02 09:07:30 +1000

    my ninth attempt to create a multipage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import './App.css';
 import Shapes from './components/shapes/shapes'
 import Logos from './components/logos/logos'
@@ -20,6 +21,11 @@ function App({ headline, showLogo, backgroundImage }) {
   );
 }
 
+App.propTypes = {
+  headline: PropTypes.string,
+  showLogo: PropTypes.bool,
+  backgroundImage: PropTypes.string,
+}
 
 App.defaultProps = {
   headline: 'I am a cool World',
