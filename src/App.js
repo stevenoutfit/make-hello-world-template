0964582719d71d10de1bd0b608d3904fd78b1435
Steven <steven@outfit.io>
2020-11-02 09:19:53 +1000

    my ninth attempt to create a multipage

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import {ReactComponent as MakeLogo} from './logo.svg'
 import {ReactComponent as ReactLogo} from './react-logo.svg'
 
 function App({ headline, showLogo, backgroundImage }) {
+
+  const headlineCheck = JSON.parse(headline);
+
   return (
     <div className="App" style={{ backgroundImage: `url('${backgroundImage}')` }}>
       {showLogo && (
         <Logos logoOne={<MakeLogo />} logoTwo={<ReactLogo />} />
       )}
 
+      {headlineCheck ? <h1>{headline}</h1> : <h1>"I am Steveee's world"</h1>}
       <h1>{headline}</h1>
 
       <div className="shape-container">
@@ -25,12 +29,6 @@ App.propTypes = {
   headline: PropTypes.string,
   showLogo: PropTypes.bool,
   backgroundImage: PropTypes.string,
-}
-
-App.defaultProps = {
-  headline: 'I am a cool World',
-  showLogo: true,
-  backgroundImage: '',
 }
 
 export default App;
